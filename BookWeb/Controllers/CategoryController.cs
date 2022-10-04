2a67d1dac8f2f0b71062e4aaedb610dd159e8465
MasriNaser <naser.masri@cronos.be>
2022-10-04 07:56:43 +0200

    init commit

---
+++ BookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,124 @@
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        //To use Http POST request.
+        //The ValidateAntiForgeryToken attribute is used to prevent cross-site request forgery attacks
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The Display Order cannot exatly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Create successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //edit && edi...../////////////////////////////////////////////////////////////////
+        //GET
+        public IActionResult Edit(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            //var CategoryFromDbFirst = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //var categoryFromSingle= _db.Categories.SingleOrDefault(x => x.Id == id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        //POST
+
+        [HttpPost]
+        //To use Http POST request.
+        //The ValidateAntiForgeryToken attribute is used to prevent cross-site request forgery attacks
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "THe Display Order cannot exatly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        /////////////////////////////////Delete///////////////////////////////////////
+        //GET
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            //var CategoryFromDbFirst = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //var categoryFromSingle= _db.Categories.SingleOrDefault(x => x.Id == id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        //We can use [ActionName("Delete")]
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        //POST
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data;
+
+public class ApplicationDbContext :DbContext
+{
+    //create a contracture
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> t) : base(t)
+    {
+        Console.WriteLine("this is the options"+t);
+    }
+    public DbSet<Category> Categories { get; set; }
+}
