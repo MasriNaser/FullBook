bf39d312a1efcf17c05e1a377fa9c6d604b74cb4
MasriNaser <naser.masri@cronos.be>
2022-10-05 14:16:23 +0200

    added ignore file

--- BookWeb/Models/Category.cs
+++ BookWeb/Models/Category.cs
@@ -1,20 +1,16 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿namespace BookWeb.Models;
 
-namespace BookWeb.Models
+public class Category
 {
-    public class Category
-    {
-        //add annotation for validation
-        [Key]
-        //add properties
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage ="Display Order must be 1 to 100 only.")]
-        public int DisplayOrder { get; set; }
-        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    //add annotation for validation
+    [Key]
+    //add properties
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1,100,ErrorMessage ="Display Order must be 1 to 100 only.")]
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
-    }
 }
